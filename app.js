88f46a5480c523c4996846dfb1fd7d19f9e016bd
Shubham Singh <shubhamsng7@gmail.com>
2021-06-19 11:05:52 +0530

    feat: birthday greeting

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const days =  parseInt(difference / (1000 * 3600 * 24));
     birthdayCount.innerHTML = days;
     caption.innerHTML = 'days to go!';
+    if(days === 0) {
+        birthdayCount.innerHTML = '';
+        caption.innerHTML = 'Happy Birthday Saanvi!';
+    }
     document.body.appendChild(birthdayCount);
     document.body.appendChild(caption);
 });
