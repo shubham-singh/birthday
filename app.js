29680726df99bfbeec2aa74385ea3092939556a2
Shubham Singh <shubhamsng7@gmail.com>
2023-01-01 23:17:46 +0530

    add comment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const body = document.getElementsByTagName('body')[0]
-    
+
     const url = new URL(window.location.href)
     const searchParams = url.searchParams
 
@@ -18,33 +18,33 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.appendChild(h3)
         return;
     }
-     
+
     let date, DD, MM, name
 
     date = searchParams.get('date')
     DD = date.split('-')[0]
-    MM = Number(date.split('-')[1]) - 1
-    
+    MM = Number(date.split('-')[1]) - 1 // Month index starts from 0
+
     const birthdayCount = document.createElement('h1');
     const caption = document.createElement('h2');
     const today = new Date();
     const currentYear = today.getFullYear()
 
     let endDate = new Date(currentYear, MM, DD, 0, 0, 0);
-    let difference = endDate - today 
+    let difference = endDate - today
 
     while (difference < -(1000 * 3600 * 24)) {
         endDate = new Date(currentYear + 1, MM, DD, 0, 0, 0)
         difference = endDate - today
     }
-    
-    const days =  parseInt(difference / (1000 * 3600 * 24));
+
+    const days = parseInt(difference / (1000 * 3600 * 24));
     birthdayCount.innerHTML = days;
-    
+
     name = searchParams.get('name')
     caption.innerHTML = `days to go ${name}!`
 
-    if(days === 0) {
+    if (days === 0) {
         birthdayCount.innerHTML = '';
         caption.innerHTML = `happy birthday ${name}!`;
     }
@@ -60,6 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.title = `${name}'s birthday countdown`
 
     if (searchParams.has('cleanup')) {
-        history.replaceState('','',window.location.origin);
+        history.replaceState('', '', window.location.origin);
     }
 });
